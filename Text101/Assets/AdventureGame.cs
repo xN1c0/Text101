e4eaf95493cb1406ed0191aa9fdb13caaaa24ad4
xNiC0 <45330099+xNiC0@users.noreply.github.com>
2019-05-15 23:04:24 +0200

    ScriptableObject + TextBox
    [SerializeField]
    [CreateAssetMenu(menuName = "State")]
    ScriptableObject

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        Debug.Log(startingState);
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Text101/Assets/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    [TextArea(10, 40)] [SerializeField] string storyText;
+    
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
