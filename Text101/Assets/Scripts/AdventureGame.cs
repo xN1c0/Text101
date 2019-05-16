2e0241b31416d63dc56c9fd15867aa05c7e60c51
xNiC0 <45330099+xNiC0@users.noreply.github.com>
2019-05-16 18:28:45 +0200

    Magical Stick
    Finito Magical Stick
    Si dovrebbe aggiungere un po' di storia :+1:

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        Debug.Log(startingState);
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = state.GetNextStates();
+
+        for(int i=0; i < nextStates.Length; i++)
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+                state = nextStates[i];
+
+        textComponent.text = state.GetStateStory();
+    }
+}
+
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    [TextArea(10, 40)] [SerializeField] string storyText;
+    [SerializeField] State[] nextState;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextState;
+    }
+}
